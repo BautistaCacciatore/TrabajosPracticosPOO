7e5da72200d4ca972802d38b355f6111c0522971
BautistaCacciatore <bachicacciatore@gmail.com>
2023-09-06 21:29:28 -0300

    TP1

--- punto9/MainPunto9.java
+++ punto9/MainPunto9.java
@@ -1,5 +1,6 @@
 package poo.punto9;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class MainPunto9 {
@@ -7,37 +8,37 @@ public class MainPunto9 {
         Scanner sc = new Scanner(System.in);
         OperacionesConFechas opf= new OperacionesConFechas();
         System.out.println("-----TRANSFORMANDO STRING EN FECHA-----");
-        String a= opf.transformarFecha("30082023", 1);
+        LocalDate fecha1= opf.transformarFecha("15-08-2023", "dd-MM-yyyy");
         System.out.println("FORMATO DD-MM-YYYY: ");
-        System.out.println(a);
-        String b= opf.transformarFecha("30082023", 2);
+        System.out.println(fecha1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        LocalDate fecha2= opf.transformarFecha("08-15-2023", "MM-dd-yyyy");
         System.out.println("FORMATO MM-DD-YYYY: ");
-        System.out.println(b);
+        System.out.println(fecha2.format(DateTimeFormatter.ofPattern("MM-dd-yyyy")));
         System.out.println("-----AVERIGUANDO SI UNA FECHA SE ENCUENTRA ENTRE OTRAS DOS---");
-        LocalDate f1= LocalDate.of(2023, 9, 28);
-        LocalDate f2= LocalDate.of(2023, 9, 15);
-        LocalDate f3= LocalDate.of(2023, 10, 30);
+        LocalDate f1= opf.transformarFecha("28-09-2023", "dd-MM-yyyy");
+        LocalDate f2= opf.transformarFecha("15-09-2023", "dd-MM-yyyy");
+        LocalDate f3= opf.transformarFecha("30-10-2023", "dd-MM-yyyy");
         boolean correcto= opf.fechaEntreDos(f2, f3, f1);
         if (correcto == true){
-            System.out.println("LA FECHA " + f1 + " SE ENCUENTRA ENTRE " + f2 + " Y " + f3);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " SE ENCUENTRA ENTRE " + f2.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " Y " + f3.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }else{
-            System.out.println("LA FECHA " + f1 + " NO SE ENCUENTRA ENTRE " + f2 + " Y " + f3);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " NO SE ENCUENTRA ENTRE " + f2.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " Y " + f3.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }
         System.out.println("-----AVERIGUANDO SI UNA FECHA ES MAYOR A OTRA---");
         boolean mayor= opf.fechaMayor(f1, f2);
         if (mayor == true){
-            System.out.println("LA FECHA " + f1 + " ES MAYOR A " + f2);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " ES MAYOR A " + f2.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }
         else{
-            System.out.println("LA FECHA " + f1 + " NO ES MAYOR A " + f2);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " NO ES MAYOR A " + f2.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }
         System.out.println("-----AVERIGUANDO SI UNA FECHA ES MENOR A OTRA---");
         boolean menor= opf.fechaMenor(f1, f3);
         if (menor == true){
-            System.out.println("LA FECHA " + f1 + " ES MENOR A " + f3);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + " ES MENOR A " + f3.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }
         else{
-            System.out.println("LA FECHA " + f1 +  " NO ES MENOR A " + f3);
+            System.out.println("LA FECHA " + f1.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +  " NO ES MENOR A " + f3.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         }
 
     }
--- punto9/OperacionesConFechas.java
+++ punto9/OperacionesConFechas.java
@@ -1,40 +1,12 @@
 package poo.punto9;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Locale;
+
 
 public class OperacionesConFechas{
-    public String transformarFecha(String fecha, int opcion){
-        String dia="", mes="", anio="";
-        for (int i=0; i < fecha.length(); i++){
-            if (i<=1){
-                dia += fecha.charAt(i);
-            }
-            else{
-                if (i==2 || i==3){
-                    mes += fecha.charAt(i);
-                }
-                else{
-                    anio += fecha.charAt(i);
-                }
-            }
-        }
-        int diai= Integer.parseInt(dia);
-        int mesi= Integer.parseInt(mes);
-        int anioi= Integer.parseInt(anio);
-        LocalDate DDMMYYYY= LocalDate.of(anioi, mesi, diai);
-        if (opcion==1){
-            DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-            String fechaString = DDMMYYYY.format(formato); // Convierte LocalDate a String
-            return fechaString;
-        }
-        else{
-            DateTimeFormatter formato = DateTimeFormatter.ofPattern("MM-dd-yyyy");
-            String fechaString = DDMMYYYY.format(formato); // Convierte LocalDate a String
-            return fechaString;
-        }
+    public LocalDate transformarFecha(String dateString, String format){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        return LocalDate.parse(dateString, formatter);
     }
 
     public boolean fechaEntreDos(LocalDate fecha1, LocalDate fecha2, LocalDate fecha_buscada){
